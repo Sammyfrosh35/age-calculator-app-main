b47fd4999a4cfc623a288aa9253bc2f9b407eba1
Sammyfrosh35 <badejosamuel9@gmail.com>
2024-03-17 23:58:12 +0100

    HMM

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function calc() {
     let day = document.getElementById('day').value;
     let month = document.getElementById('month').value;
@@ -9,29 +10,32 @@ function calc() {
     let age = todayYear - year;
     let ageMonth = 0;
     let ageDay = 0;
-
+   
+    
 
     //  error handling
+    document.getElementsByClassName("result").textContent = "";
     document.getElementById("dayError").textContent = "";
     document.getElementById("monthError").textContent = "";
     document.getElementById("yearError").textContent = "";
-
-
     document.getElementById("ageyear").textContent = "";
     document.getElementById("agemonth").textContent = "";
     document.getElementById("ageday").textContent = "";
 
-    // validate day
-    if (day > 31) {
-        document.getElementById("dayError").textContent = "Must be a valid day";
+    // Check if input fields are empty 
+
+    if (!day || !month || !year){
+        document.getElementById('dayError').innerText = 'this field is required.';
+        document.getElementById('monthError').innerText = 'this field is required.';
+        document.getElementById('yearError').innerText = 'this field is required.';
     }
-    // validate month
-    if (month > 12) {
-        document.getElementById("monthError").textContent = "Must be a valid month";
-    }
-    // validate year
-    if (year > todayYear) {
-        document.getElementById("yearError").textContent = "Must be a valid year";
+
+
+    // validate day, month, and year
+    if (day > 31 || month > 12 || year > todayYear) {
+    document.getElementById("dayError").textContent = "must be a valid day";
+    document.getElementById("monthError").textContent = "must be a valid month";
+    document.getElementById("yearError").textContent = "must be a valid year";
     }
 
     // calculate age year
@@ -45,17 +49,12 @@ function calc() {
     }
     document.getElementById("agemonth").textContent = ageMonth;
 
-
-
     //  calculate age days
-
     if (day <= today) {
         ageDay = today - day
-    }
-    else {
+    } else {
         ageMonth = ageMonth - 1;
         ageDay = 31 - (day - today)
     }
     document.getElementById("ageday").textContent = ageDay;
-
 }
